790e42b6988552d86a3b51882e720f0238e6076f
Antoine Ceol <antoine.ceol@gmail.com>
2021-04-19 22:57:25 +0200

    Portal single config file

--- gravitee/graviteeio-portal-ui/bin/setup-config.sh
+++ gravitee/graviteeio-portal-ui/bin/setup-config.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-set -x
+
 echo -n "-- Setup portal-ui config --"
 
-FOLDER=${1:-}
-if [[ -z "${FOLDER}" ]]; then
+DIST_FOLDER=${1:-}
+if [[ -z "${DIST_FOLDER}" ]]; then
   echo -n "-----> Veuillez spécifier un répertoire i.e. '.' "
   exit 1
 fi
 
-if [[ -z "${APIM_ENV}" ]]; then
-echo -n "-----> Veuillez saisir l'environnement Gravitee à configurer en définissant la variable d'env APIM_ENV ..."
-exit 1
-fi
-
 echo -n "-----> Copying buildpack gravitee portal ui configuration ... "
-ls -ail ..
-echo "---"
-ls -ail .
-mv gravitee/graviteeio-portal-ui/pix-config/${APIM_ENV}/config.json ${FOLDER}/assets/config.json
+CONFIG_FILE=gravitee/graviteeio-portal-ui/pix-config/config.json
+
+cat $CONFIG_FILE | sed "s#MANAGEMENT_API_URL#${MANAGEMENT_API_URL}#" > ${DIST_FOLDER}/assets/config.json
+
 echo "done"
