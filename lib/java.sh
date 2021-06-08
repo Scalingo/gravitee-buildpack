9b7cbf08295c855944e73bcada9c9fec06b0a558
Soulou <leo@unbekandt.eu>
2021-06-08 13:23:58 +0200

    Install JDK 11 by default

--- lib/java.sh
+++ lib/java.sh
@@ -6,13 +6,15 @@ install_jdk() {
   set +u
 
   local install_dir="$1"
+  local cache_dir="$2"
+
   JVM_COMMON_BUILDPACK=${JVM_COMMON_BUILDPACK:-https://buildpacks-repository.s3.eu-central-1.amazonaws.com/jvm-common.tar.xz}
   mkdir -p /tmp/jvm-common
   curl --retry 3 --silent --location $JVM_COMMON_BUILDPACK | tar --extract --xz --touch -C /tmp/jvm-common --strip-components=1
   source /tmp/jvm-common/bin/util
   source /tmp/jvm-common/bin/java
   source /tmp/jvm-common/opt/jdbc.sh
-  install_java_with_overlay ${install_dir}
+  install_java_with_overlay "${install_dir}" "${cache_dir}"
 
   set -u
 }
