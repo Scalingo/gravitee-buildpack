6a1f9fb65bbe1166e2bd17091115ab44c2dcaf5c
François de Metz <francois.de-metz@pix.fr>
2022-11-16 10:02:26 +0100

    Update buildpack to be compatible with 3.5.19

--- gravitee/graviteeio-apim-rest-api/scripts/start.sh
+++ gravitee/graviteeio-apim-rest-api/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "-----> Configuring gravitee REST API"
+echo "-----> Configuring gravitee APIM REST API"
 "${HOME}/scripts/config.rb"
 
-echo "-----> Starting gravitee REST API…"
-exec bash graviteeio-rest-api/bin/gravitee
+echo "-----> Starting gravitee APIM REST API…"
+exec bash graviteeio-apim-rest-api/bin/gravitee
